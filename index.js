4b7c271db5cee64f2da3c2f2e2f913f9c9a6ae7c
Usman Akram <usman.akram@gmail.com>
2020-12-05 01:15:28 +0500

    fixed model Schema

--- index.js
+++ index.js
@@ -1,26 +1,37 @@
 // Node Packages
-const config = require('config');
-const Joi = require('joi')
-const express = require('express');
+const config = require("config");
+const mongoose = require("mongoose");
+const Joi = require("joi");
+const express = require("express");
 const app = express();
+
 // const patients = require('./routes/patients')
-const home = require('./routes/home')
+const home = require("./routes/home");
 
 // Defing that json will be used for CRUD operations
-app.use(express.json()) // req.body
-app.use(express.urlencoded({ extended: true })) // data: key
-app.use(express.static('public')) // for custom images/ css
+app.use(express.json()); // req.body
+app.use(express.urlencoded({ extended: true })); // data: key
+app.use(express.static("public")); // for custom images/ css
 
 require("./startup/api_routes")(app);
 // app.use('/patient', patients)
-app.use('/', home)
+app.use("/", home);
 
 // Configuration
 // console.log('Application Name ' + config.get('name'))
 // console.log('Application Main File ' + config.get('main'))
 
 // If port 3000 is free allocate it, else find the availbale port and allocate.
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3000;
 app.listen(port, () =>
-    // Server will run on the allocated port
-    console.log(`Server Listening on port ${port}`));
+  // Server will run on the allocated port
+  console.log(`Server Listening on port ${port}`)
+);
+
+mongoose
+  .connect(config.get("db"), {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("Connected to Mongoose"))
+  .catch(() => console.log("Could not connected to mongoDB.."));
--- models/PatientModel.js
+++ models/PatientModel.js
@@ -1,89 +1,83 @@
 const mongoose = require("mongoose");
-const Joi = require('joi')
-const config = require('config');
-
-mongoose.connect(config.get("db"), {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-    })
-    .then(() => console.log("Connected to Mongoose"))
-    .catch(() => console.log("Could not connected to mongoDB.."))
+const Joi = require("joi");
 
 const patientSchema = new mongoose.Schema({
-    patientName: String,
-    patientDescription: String
-})
+  patientName: String,
+  patientDescription: String,
+});
 
-const PatientModel = mongoose.model('PatientDb', patientSchema);
-
+const PatientModel = mongoose.model("PatientDb", patientSchema);
 
 // Its a static Method which can be called as Patient.doStuff();
-modelSchema.statics.getPatientById = async function(PatientId) {
-    // Validating if given user is in our List
-    //const patient = patients.find(p => p.id === parseInt(PatientId));
-    // If not existing, return 404
-    return patient = await PatientModel.findById(PatientId, function(err, patient) {
-        if (err) return 'Error 404 No patient with the given id in the record';
-        else return patient;
-    });
+patientSchema.statics.getPatientById = async function (PatientId) {
+  // Validating if given user is in our List
+  //const patient = patients.find(p => p.id === parseInt(PatientId));
+  // If not existing, return 404
+  return (patient = await PatientModel.findById(
+    PatientId,
+    function (err, patient) {
+      if (err) return "Error 404 No patient with the given id in the record";
+      else return patient;
+    }
+  ));
+};
 
-}
+patientSchema.statics.getPatient = async function () {
+  var result = [];
+  const patient_Obj_Result = await PatientModel.find();
+  patient_Obj_Result.forEach(function (doc, err) {
+    result.push(doc);
+  });
+  console.log(result);
+  return result;
+};
 
-modelSchema.statics.getPatient = async function() {
-    var result = []
-    const patient_Obj_Result = await PatientModel.find();
-    patient_Obj_Result.forEach(function(doc, err) {
-        result.push(doc)
-    })
-    console.log(result)
-    return result
-}
+patientSchema.statics.getPage = async function (page = 1, perPage = 10) {
+  return this.find()
+    .limit(perPage)
+    .skip((page - 1) * perPage);
+};
 
-modelSchema.statics.getPage = async function(page = 1, perPage = 10) {
-    return this.find().limit(perPage).skip((page - 1) * perPage);
-}
-
-modelSchema.statics.validate = async function(RequestedBody) {
-    //  Validating
-    return { error } = validatePatient(RequestedBody);
-}
+patientSchema.statics.validate = async function (RequestedBody) {
+  //  Validating
+  return ({ error } = validatePatient(RequestedBody));
+};
 
 //can be called on instance like. let p = new Patient(); p.doStuffOnSIngleRecord();
 //dont use arrow functions here
-modelSchema.methods.addPatient = async function(PatientName) {
-    // Add patient
-    const Patient_Obj = new PatientModel({
-        patientName: PatientName.name,
-        patientDescription: PatientName.description
-    });
+patientSchema.methods.addPatient = async function (PatientName) {
+  // Add patient
+  const Patient_Obj = new PatientModel({
+    patientName: PatientName.name,
+    patientDescription: PatientName.description,
+  });
 
-    await Patient_Obj.save();
-    var result = []
-    const product_Obj_Result = await PatientModel.find();
-    product_Obj_Result.forEach(function(doc, err) {
-        result.push(doc)
-    })
-    return result;
-
-}
-modelSchema.virtual("annualSalary").get(function() {
-    return this.salary * 12;
+  await Patient_Obj.save();
+  var result = [];
+  const product_Obj_Result = await PatientModel.find();
+  product_Obj_Result.forEach(function (doc, err) {
+    result.push(doc);
+  });
+  return result;
+};
+patientSchema.virtual("annualSalary").get(function () {
+  return this.salary * 12;
 });
 
-modelSchema.post("save", async(doc) => {
-    //https://mongoosejs.com/docs/middleware.html
-    //this method will be called when a save is successful on a single record
+patientSchema.post("save", async (doc) => {
+  //https://mongoosejs.com/docs/middleware.html
+  //this method will be called when a save is successful on a single record
 });
 
 function validatePatient(patient) {
-    // Designing JOI Validation schema
-    const schema = Joi.object({
-        name: Joi.string().min(3).required(),
-    });
-    // Returniing the resuslt
-    return schema.validate(patient);
+  // Designing JOI Validation schema
+  const schema = Joi.object({
+    name: Joi.string().min(3).required(),
+  });
+  // Returniing the resuslt
+  return schema.validate(patient);
 }
 
-modelSchema.set("toJSON", { virtuals: true });
-const Model = mongoose.model("Patient", modelSchema);
+patientSchema.set("toJSON", { virtuals: true });
+const Model = mongoose.model("Patient", patientSchema);
 module.exports = Model;
