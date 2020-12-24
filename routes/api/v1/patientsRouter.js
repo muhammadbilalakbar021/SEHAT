704425e15992d1608aec96e0631b0cff5dfbea7c
muhammadbilalakbar021 <muhammadbilalakbar021@gmail.com>
2020-12-24 20:02:25 +0500

    Update patientsRouter.js
    change get to post "login by emai password"

--- routes/api/v1/patientsRouter.js
+++ routes/api/v1/patientsRouter.js
@@ -5,41 +5,42 @@ const Patient = require("../../../models/PatientModel");
 const patientValidator = require("../../../middlewares/validators/patientValidator");
 
 // Get request for returning all patient's list
-router.get("/", async (req, res) => {
-  const patientsRecord = await Patient.getPatient();
-  res.send(patientsRecord);
+router.get("/", async(req, res) => {
+    const patientsRecord = await Patient.getPatient();
+    res.send(patientsRecord);
 });
 
 // Get request for returning patient by login
-router.get("/login", async (req, res) => {
-  const patientsRecord = await Patient.getPatientByEmailPasscode(req.body);
-  res.send(patientsRecord);
+router.post("/login", async(req, res) => {
+    const patientsRecord = await Patient.getPatientByEmailPasscode(req.body);
+    res.send(patientsRecord);
 });
 
 // Get request for returning specific patient
-router.get("/patient/:id", async (req, res) => {
-  patient = await Patient.getPatientById(req.params.id);
-  // If Patient exist, return patient record
-  res.send(patient);
+router.get("/patientById/:id", async(req, res) => {
+    patient = await Patient.getPatientById(req.params.id);
+    // If Patient exist, return patient record
+    console.log("hello")
+    res.send(patient);
 });
 
 // Post request for adding paatient
-router.post("/", patientValidator, async (req, res) => {
-  patients = new Patient();
-  p = await patients.addPatient(req.body);
-  // Return Patient list
-  res.send(p);
+router.post("/", patientValidator, async(req, res) => {
+    patients = new Patient();
+    p = await patients.addPatient(req.body);
+    // Return Patient list
+    res.send(p);
 });
 
-router.get("/:page?/:perPage?", async (req, res) => {
-  const page = req.params.page ? Number(req.params.page) : 1;
-  const perPage = req.params.perPage ? Number(req.params.perPage) : 1;
-  //Bad Way
-  //const patients = await Patient.find().limit(perPage).skip((page - 1) * perPage);
+router.get("/:page?/:perPage?", async(req, res) => {
+    const page = req.params.page ? Number(req.params.page) : 1;
+    const perPage = req.params.perPage ? Number(req.params.perPage) : 1;
+    //Bad Way
+    //const patients = await Patient.find().limit(perPage).skip((page - 1) * perPage);
 
-  //Good Way
-  const patients = await Patient.getPage(page, perPage);
-  res.send(patients);
+    //Good Way
+    const patients = await Patient.getPage(page, perPage);
+    res.send(patients);
 });
 
 module.exports = router;
