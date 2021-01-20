4d6e3e97a209c1936b5fe7748a0de19356d45b20
Mian442 <62568869+Mian442@users.noreply.github.com>
2021-01-20 16:50:29 +0500

    Fix for dublicate email middle ware

--- routes/api/v1/patientsRouter.js
+++ routes/api/v1/patientsRouter.js
@@ -1,43 +1,50 @@
 const express = require("express");
 const router = express.Router();
 const Joi = require("joi");
-const Patient = require("../../../models/PatientModel");
+const { PatientSchema } = require("../../../models/PatientModel");
 const patientValidator = require("../../../middlewares/validators/patientValidator");
 const duplicateEmailValidator = require("../../../middlewares/validators/duplicateEmailValidator");
 
 // Get request for returning all patient's list
-router.get("/", async(req, res) => {
-    const patientsRecord = await Patient.getPatient();
-    res.send(patientsRecord);
+router.get("/", async (req, res) => {
+  const patientsRecord = await PatientSchema.getPatient();
+  res.send(patientsRecord);
 });
 
 // Get request for returning patient by login
-router.post("/login", async(req, res) => {
-    const patientsRecord = await Patient.getPatientByEmailPasscode(req.body);
-    res.send(patientsRecord);
+router.post("/login", async (req, res) => {
+  const patientsRecord = await PatientSchema.getPatientByEmailPasscode(
+    req.body
+  );
+  res.send(patientsRecord);
 });
 
 // Get request for returning specific patient
-router.get("/patientById/:id", async(req, res) => {
-    patient = await Patient.getPatientById(req.params.id);
-    // If Patient exist, return patient record
-    res.send(patient);
+router.get("/patientById/:id", async (req, res) => {
+  patient = await PatientSchema.getPatientById(req.params.id);
+  // If Patient exist, return patient record
+  res.send(patient);
 });
 
 // Post request for adding paatient
-router.post("/", patientValidator, duplicateEmailValidator, async(req, res) => {
-    patients = new Patient();
+router.post(
+  "/",
+  patientValidator,
+  duplicateEmailValidator,
+  async (req, res) => {
+    patients = new PatientSchema();
     p = await patients.addPatient(req.body);
     // Return Patient list
     res.send(p);
-});
+  }
+);
 
-router.get("/:page?/:perPage?", async(req, res) => {
-    const page = req.params.page ? Number(req.params.page) : 1;
-    const perPage = req.params.perPage ? Number(req.params.perPage) : 1;
+router.get("/:page?/:perPage?", async (req, res) => {
+  const page = req.params.page ? Number(req.params.page) : 1;
+  const perPage = req.params.perPage ? Number(req.params.perPage) : 1;
 
-    const patients = await Patient.getPage(page, perPage);
-    res.send(patients);
+  const patients = await PatientSchema.getPage(page, perPage);
+  res.send(patients);
 });
 
 module.exports = router;
