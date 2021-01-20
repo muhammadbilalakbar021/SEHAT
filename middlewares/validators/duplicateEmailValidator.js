12229fd4c06ec1984a5bcac3c1bcf6d70425eac7
muhammadbilalakbar021 <muhammadbilalakbar021@gmail.com>
2021-01-20 17:02:34 +0500

    Update duplicateEmailValidator.js

--- middlewares/validators/duplicateEmailValidator.js
+++ middlewares/validators/duplicateEmailValidator.js
@@ -1,15 +1,12 @@
 const { func } = require("joi");
 let { PatientModel } = require("../../models/PatientModel");
-module.exports = async function (req, res, next) {
-  PatientModel.findOne({ emailAddress: req.body.emailAddress }).then(function (
-    user
-  ) {
-    console.log(user);
-    // if (user) {
-    //   console.log(user);
-    //   res.status(400).send("Email has been already registered");
-    // }
-  });
-  req.isValidated = true;
-  next();
+module.exports = async function(req, res, next) {
+    PatientModel.findOne({ emailAddress: req.body.emailAddress }).then(
+        function(user) {
+            if (user) {
+                res.status(400).send("Email has been already registered");
+            }
+        });
+    req.isValidated = true;
+    next();
 };
