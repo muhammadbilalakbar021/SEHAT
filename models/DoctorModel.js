e60f5bcec90f36a245ce032328978fc1188f28e1
Mian442 <62568869+Mian442@users.noreply.github.com>
2021-02-04 11:34:27 +0500

    minor fixes #added patient send response

--- models/DoctorModel.js
+++ models/DoctorModel.js
@@ -5,120 +5,110 @@ const { PatientModel } = require("./PatientModel");
 var sess_; // global session, NOT recommended
 
 const doctorSchema = new mongoose.Schema({
-    rating: Number,
-    mainofficeaddress: String,
-    review: Array,
-    qualification: Array,
-    services: Array,
-    workexperence: Array,
-    experties: Array,
-    speciality: Array
-
+  rating: Number,
+  mainofficeaddress: String,
+  review: Array,
+  qualification: Array,
+  services: Array,
+  workexperence: Array,
+  experties: Array,
+  speciality: Array,
 });
 const doctorModel = mongoose.model("doctorDb", doctorSchema);
 
 // Its a static Method which can be called as doctor.doStuff();
-doctorSchema.statics.getdoctorById = async function(doctorId) {
-    // Validating if given user is in our List
-    //const doctor = doctors.find(p => p.id === parseInt(doctorId));
-    // If not existing, return 404
-    return (doctor = await doctorModel.findById(
-        doctorId,
-        function(err, doctor) {
-            if (err) return "Error 404 No doctor with the given id in the record";
-            else return doctor;
-        }
-    ));
+doctorSchema.statics.getdoctorById = async function (doctorId) {
+  // Validating if given user is in our List
+  //const doctor = doctors.find(p => p.id === parseInt(doctorId));
+  // If not existing, return 404
+  return (doctor = await doctorModel.findById(doctorId, function (err, doctor) {
+    if (err) return "Error 404 No doctor with the given id in the record";
+    else return doctor;
+  }));
 };
 
-doctorSchema.statics.addServices = async function(RequestedBody) {
-
-    let doctor = await PatientModel.findById(RequestedBody.id)
-        // doctor.isDoctor.services = doctor.isDoctor.services ? [...doctor.isDoctor.services] : []
-    doctor.isDoctor.services = RequestedBody.services
-    doctor.markModified("isDoctor")
-    doctor.save()
-    return doctor
+doctorSchema.statics.addServices = async function (RequestedBody) {
+  let doctor = await PatientModel.findById(RequestedBody.id);
+  doctor.isDoctor.services = RequestedBody.services;
+  doctor.markModified("isDoctor");
+  await doctor.save();
+  return doctor;
 };
 
-doctorSchema.statics.review = async function(RequestedBody) {
+doctorSchema.statics.review = async function (RequestedBody) {
+  let doctor = await PatientModel.findById(RequestedBody.id);
+  let updateDynamicSchema = {
+    date: RequestedBody.date,
+    pic: RequestedBody.pic,
+    userName: RequestedBody.userName,
+    patientId: RequestedBody.patientId,
+    Star: RequestedBody.Star,
+    comment: RequestedBody.comment,
+  };
 
-    let doctor = await PatientModel.findById(RequestedBody.id)
-    let updateDynamicSchema = {
-        date: RequestedBody.date,
-        pic: RequestedBody.pic,
-        userName: RequestedBody.userName,
-        patientId: RequestedBody.patientId,
-        Star: RequestedBody.Star,
-        comment: RequestedBody.comment
-    };
-
-    doctor.isDoctor.review = doctor.isDoctor.review ? [...doctor.isDoctor.review] : []
-    doctor.isDoctor.review.push(updateDynamicSchema)
-    doctor.markModified("isDoctor")
-    doctor.save()
-    return doctor
+  doctor.isDoctor.review = doctor.isDoctor.review
+    ? [...doctor.isDoctor.review]
+    : [];
+  doctor.isDoctor.review.push(updateDynamicSchema);
+  doctor.markModified("isDoctor");
+  doctor.save();
+  return doctor;
 };
 
-doctorSchema.statics.qualification = async function(RequestedBody) {
-
-    let doctor = await PatientModel.findById(RequestedBody.id)
-    doctor.isDoctor.qualification = RequestedBody.qualification
-    doctor.markModified("isDoctor")
-    doctor.save()
-    return doctor
+doctorSchema.statics.qualification = async function (RequestedBody) {
+  let doctor = await PatientModel.findById(RequestedBody.id);
+  doctor.isDoctor.qualification = RequestedBody.qualification;
+  doctor.markModified("isDoctor");
+  doctor.save();
+  return doctor;
 };
 
-doctorSchema.statics.workexperence = async function(RequestedBody) {
-
-    let doctor = await PatientModel.findById(RequestedBody.id)
-    doctor.isDoctor.workexperence = RequestedBody.workexperence
-    doctor.markModified("isDoctor")
-    doctor.save()
-    return doctor
+doctorSchema.statics.workexperence = async function (RequestedBody) {
+  let doctor = await PatientModel.findById(RequestedBody.id);
+  doctor.isDoctor.workexperence = RequestedBody.workexperence;
+  doctor.markModified("isDoctor");
+  doctor.save();
+  return doctor;
 };
 
-doctorSchema.statics.experties = async function(RequestedBody) {
-
-    let doctor = await PatientModel.findById(RequestedBody.id)
-    doctor.isDoctor.experties = RequestedBody.experties
-    doctor.markModified("isDoctor")
-    doctor.save()
-    return doctor
+doctorSchema.statics.experties = async function (RequestedBody) {
+  let doctor = await PatientModel.findById(RequestedBody.id);
+  doctor.isDoctor.experties = RequestedBody.experties;
+  doctor.markModified("isDoctor");
+  doctor.save();
+  return doctor;
 };
 
-doctorSchema.statics.achievments = async function(RequestedBody) {
-
-    let doctor = await PatientModel.findById(RequestedBody.id)
-    doctor.isDoctor.speciality = RequestedBody.speciality
-    doctor.markModified("isDoctor")
-    doctor.save()
-    return doctor
+doctorSchema.statics.achievments = async function (RequestedBody) {
+  let doctor = await PatientModel.findById(RequestedBody.id);
+  doctor.isDoctor.speciality = RequestedBody.speciality;
+  doctor.markModified("isDoctor");
+  doctor.save();
+  return doctor;
 };
 
-doctorSchema.statics.publications = async function(RequestedBody) {
-
-    let doctor = await PatientModel.findById(RequestedBody.id)
-    doctor.isDoctor.publications = RequestedBody.publications
-    doctor.markModified("isDoctor")
-    doctor.save()
-    return doctor
+doctorSchema.statics.publications = async function (RequestedBody) {
+  let doctor = await PatientModel.findById(RequestedBody.id);
+  doctor.isDoctor.publications = RequestedBody.publications;
+  doctor.markModified("isDoctor");
+  doctor.save();
+  return doctor;
 };
 
 function validatedoctor(doctor) {
-    // Designing JOI Validation schema
-    const schema = Joi.object({
-        fname: Joi.string().min(3).required(),
-        lname: Joi.string().min(3).required(),
-        title: Joi.string().min(2).required(),
-        DOB: Joi.string().min(3).required(),
-        pic: Joi.string().required(),
-        Ph: Joi.number().required(),
-        emailAddress: Joi.string().email().min(6).required(),
-        password: Joi.string().min(5).required(),
-    });
-    // Returniing the resuslt
-    return schema.validate(doctor, { abortEarly: false });
+  // Designing JOI Validation schema
+  const schema = Joi.object({
+    fname: Joi.string().min(3).required(),
+    lname: Joi.string().min(3).required(),
+    title: Joi.string().min(2).required(),
+    DOB: Joi.string().min(3).required(),
+    pic: Joi.string().required(),
+    Ph: Joi.number().required(),
+    emailAddress: Joi.string().email().min(6).required(),
+    password: Joi.string().min(5).required(),
+  });
+  // Returniing the resuslt
+  return schema.validate(doctor, { abortEarly: false });
 }
 
 doctorSchema.set("toJSON", { virtuals: true });
--- routes/api/v1/doctorRouter.js
+++ routes/api/v1/doctorRouter.js
@@ -6,60 +6,53 @@ const Doctor = require("../../../models/DoctorModel");
 const idValidator = require("../../../middlewares/validators/idValidator");
 
 // Get request for returning all patient's list
-router.get("/", async(req, res) => {
-    // const patientsRecord = await Patient.getPatient();
+router.get("/", async (req, res) => {
+  // const patientsRecord = await Patient.getPatient();
 });
 
 // Get request for returning specific doctor
-router.get("/doctorById/:id", async(req, res) => {
-    patient = await Patient.getPatientById(req.params.id);
-    // If Patient exist, return doctor record
-    console.log("hello")
-    res.send(patient);
+router.get("/doctorById/:id", async (req, res) => {
+  patient = await Patient.getPatientById(req.params.id);
+  // If Patient exist, return doctor record
+  console.log("hello");
+  res.send(patient);
 });
 
 // Services (Post)
 // doctorIdValidator, serviceValidator,
-router.post("/AddServices", idValidator, async(req, res) => {
-    doctor = await Doctor.addServices(req.body)
-    res.send(doctor)
+router.post("/AddServices", idValidator, async (req, res) => {
+  doctor = await Doctor.addServices(req.body);
+  res.send(doctor);
+});
 
-})
+router.post("/AddReview", idValidator, async (req, res) => {
+  doctor = await Doctor.review(req.body);
+  res.status(200).send(doctor);
+});
 
-router.post("/AddReview", idValidator, async(req, res) => {
-    doctor = await Doctor.review(req.body)
-    res.send(doctor)
+router.post("/AddQualification", idValidator, async (req, res) => {
+  doctor = await Doctor.qualification(req.body);
+  res.send(doctor);
+});
 
-})
+router.post("/AddWorkExperience", idValidator, async (req, res) => {
+  doctor = await Doctor.workexperence(req.body);
+  res.send(doctor);
+});
 
-router.post("/AddQualification", idValidator, async(req, res) => {
-    doctor = await Doctor.qualification(req.body)
-    res.send(doctor)
+router.post("/AddExpertise", idValidator, async (req, res) => {
+  doctor = await Doctor.experties(req.body);
+  res.send(doctor);
+});
 
-})
+router.post("/AddAchievements", idValidator, async (req, res) => {
+  doctor = await Doctor.achievments(req.body);
+  res.send(doctor);
+});
 
-router.post("/AddWorkexperence", idValidator, async(req, res) => {
-    doctor = await Doctor.workexperence(req.body)
-    res.send(doctor)
-
-})
-
-router.post("/AddExperties", idValidator, async(req, res) => {
-    doctor = await Doctor.experties(req.body)
-    res.send(doctor)
-
-})
-
-router.post("/AddAchievments", idValidator, async(req, res) => {
-    doctor = await Doctor.achievments(req.body)
-    res.send(doctor)
-
-})
-
-router.post("/AddPublications", idValidator, async(req, res) => {
-    doctor = await Doctor.publications(req.body)
-    res.send(doctor)
-
-})
+router.post("/AddPublications", idValidator, async (req, res) => {
+  doctor = await Doctor.publications(req.body);
+  res.send(doctor);
+});
 
 module.exports = router;
