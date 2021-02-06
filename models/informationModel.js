7b06528739aa34e2022a8aab4f8bc8435e1fdfc1
muhammadbilalakbar021 <muhammadbilalakbar021@gmail.com>
2021-02-06 23:04:35 +0500

    updated

--- models/informationModel.js
+++ models/informationModel.js
@@ -3,87 +3,73 @@ const Joi = require("joi");
 const { boolean } = require("joi");
 
 const userInformationSchema = new mongoose.Schema({
-  blood: String,
-  height: String,
-  martial_status: String,
-  address: String,
-  cnic: String,
-  userid: String,
-  isCreated: {
-    type: Boolean,
-    default: true,
-  },
+    blood: String,
+    height: String,
+    martial_status: String,
+    address: String,
+    cnic: String,
+    userid: String,
+    isCreated: {
+        type: Boolean,
+        default: true,
+    },
 });
 
-userInformationSchema.statics.getUserInformationById = async (userid) => {
-  let info = await UserInformationModel.findOne({ userid });
-  return info;
+userInformationSchema.statics.getUserInformationById = async(userid) => {
+    let info = await UserInformationModel.findOne({ userid });
+    return info;
 };
 
-userInformationSchema.methods.addUserInformation = async function (user) {
-  const userInformation_Obj = new UserInformationModel({
-    blood: user.blood,
-    height: user.height,
-    martial_status: user.martial_status,
-    address: user.address,
-    cnic: user.cnic,
-    userid: user.id,
-  });
+userInformationSchema.methods.addUserInformation = async function(user) {
+    const userInformation_Obj = new UserInformationModel({
+        blood: user.blood,
+        height: user.height,
+        martial_status: user.martial_status,
+        address: user.address,
+        cnic: user.cnic,
+        userid: user.id,
+    });
 
-  let info = await userInformation_Obj.save();
-  return info;
+    let info = await userInformation_Obj.save();
+    return info;
 };
 
-userInformationSchema.statics.updateUserInformation = async function (user) {
-  let info = await UserInformationModel.findOne({ userid: user.id });
-  info.blood = user.blood;
-  info.height = user.height;
-  info.martial_status = user.martial_status;
-  info.address = user.address;
-  info.cnic = user.cnic;
-  info.userid = user.id;
-  info = await info.save();
-  return info;
+userInformationSchema.statics.updateUserInformation = async function(user) {
+    let info = await UserInformationModel.findOne({ userid: user.id });
+    info.blood = user.blood;
+    info.height = user.height;
+    info.martial_status = user.martial_status;
+    info.address = user.address;
+    info.cnic = user.cnic;
+    info.userid = user.id;
+    info = await info.save();
+    return info;
 };
 
-userInformationSchema.statics.ValidateUserInformation = async function (
-  RequestedBody
+userInformationSchema.statics.ValidateUserInformation = async function(
+    RequestedBody
 ) {
-  //  Validating
-  return validate(RequestedBody);
+    //  Validating
+    return validate(RequestedBody);
 };
 
 function validate(user) {
-  // Designing JOI Validation schema
-  const schema = Joi.object({
-    id: Joi.string().required(),
-    blood: Joi.string().required(),
-    height: Joi.number().required(),
-    martial_status: Joi.string().min(2).required(),
-    address: Joi.string().min(8).required(),
-    cnic: Joi.number().required(),
-  });
-  // Returniing the resuslt
-  return schema.validate(user, { abortEarly: false });
+    // Designing JOI Validation schema
+    const schema = Joi.object({
+        id: Joi.string().required(),
+        blood: Joi.string().required(),
+        height: Joi.number().required(),
+        martial_status: Joi.string().min(2).required(),
+        address: Joi.string().min(8).required(),
+        cnic: Joi.number().required(),
+    });
+    // Returniing the resuslt
+    return schema.validate(user, { abortEarly: false });
 }
 
 userInformationSchema.set("toJSON", { virtuals: true });
-<<<<<<< HEAD
 const UserInformationModel = mongoose.model(
-  "informationdbs",
-  userInformationSchema
+    "informationdbs",
+    userInformationSchema
 );
 module.exports = UserInformationModel;
-=======
-
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-module.exports = UserModel;
-=======
-module.exports = userInformationSchema;
->>>>>>> 2c9513a730d0e39c9e2bc7d00b7c7e8ab459cd0b
->>>>>>> Stashed changes
-=======
-module.exports = userInformationSchema;
->>>>>>> 2c9513a730d0e39c9e2bc7d00b7c7e8ab459cd0b
->>>>>>> Stashed changes
--- routes/api/v1/userInformationRouter.js
+++ routes/api/v1/userInformationRouter.js
@@ -4,64 +4,43 @@ const UserInformationValidator = require("../../../middlewares/validators/user/U
 const UserInformationModel = require("../../../models/InformationModel");
 
 // Get request for returning specific patient
-router.get("/:id", async (req, res) => {
-  try {
-    information = await UserInformationModel.getUserInformationById(
-      req.params.id
-    );
-    // If Patient exist, return patient record
-    return res.status(200).send(information);
-  } catch (err) {
-    res.status(400).send("Error from UserInformation by Id!");
-  }
+router.get("/:id", async(req, res) => {
+    try {
+        information = await UserInformationModel.getUserInformationById(
+            req.params.id
+        );
+        // If Patient exist, return patient record
+        return res.status(200).send(information);
+    } catch (err) {
+        res.status(400).send("Error from UserInformation by Id!");
+    }
 });
 //Create a new one
 router.post(
-  "/add-user-information",
-  UserInformationValidator,
-  async (req, res) => {
-    try {
-      information = new UserInformationModel();
-      user_info = await information.addUserInformation(req.body);
-      return res.status(200).send(user_info);
-    } catch (err) {
-      return res.status(400).send("Error from addUserInformation");
+    "/add-user-information",
+    UserInformationValidator,
+    async(req, res) => {
+        try {
+            information = new UserInformationModel();
+            user_info = await information.addUserInformation(req.body);
+            return res.status(200).send(user_info);
+        } catch (err) {
+            return res.status(400).send("Error from addUserInformation");
+        }
     }
-  }
 );
 
-<<<<<<< HEAD
 router.put(
-  "/add-user-information",
-  UserInformationValidator,
-  async (req, res) => {
-    try {
-      console.log("dd");
-      user_info = await UserInformationModel.updateUserInformation(req.body);
-      return res.status(200).send(user_info);
-    } catch (err) {
-      return res.status(400).send("Error from updateUserInformation");
-=======
-router.post(
-    "/addUserInformation", async(req, res) => {
-        // try {
-
-        // } catch (err) {
-        //     res.status(400).json("Error from addUserInformation")
-        // }
-        infromation = new userInformationSchema()
-        userInformation = infromation.addUserInfomration(req.body)
-        res.send(userInformation);
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-=======
->>>>>>> 2c9513a730d0e39c9e2bc7d00b7c7e8ab459cd0b
->>>>>>> Stashed changes
-=======
->>>>>>> 2c9513a730d0e39c9e2bc7d00b7c7e8ab459cd0b
->>>>>>> Stashed changes
-    }
-  }
-);
+    "/add-user-information",
+    UserInformationValidator,
+    async(req, res) => {
+        try {
+            console.log("dd");
+            user_info = await UserInformationModel.updateUserInformation(req.body);
+            return res.status(200).send(user_info);
+        } catch (err) {
+            return res.status(400).send("Error from updateUserInformation");
+        }
+    })
 
 module.exports = router;
