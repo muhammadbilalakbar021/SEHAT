6a1412e15265118178ea19433bf33ce3dd89171c
Mian442 <62568869+Mian442@users.noreply.github.com>
2021-02-08 11:43:17 +0500

    Update UserMedicalHistoryRouter.js

--- routes/api/v1/UserMedicalHistoryRouter.js
+++ routes/api/v1/UserMedicalHistoryRouter.js
@@ -4,58 +4,61 @@ const UserMedicalHistoryValidator = require("../../../middlewares/validators/use
 const UserMedicalHistoryModel = require("../../../models/medicalHistoryModel");
 
 // Get request for returning specific patient
-router.get("/:id", async(req, res) => {
-    try {
-        information = await UserMedicalHistoryModel.getUserMedicalHistoryById(
-            req.params.id
-        );
-        // If Patient exist, return patient record
-        return res.status(200).send(information);
-    } catch (err) {
-        res.status(400).send("Error from UserMedicalHistory by Id!");
-    }
+router.get("/:id", async (req, res) => {
+  try {
+    information = await UserMedicalHistoryModel.getUserMedicalHistoryById(
+      req.params.id
+    );
+    // If Patient exist, return patient record
+    return res.status(200).send(information);
+  } catch (err) {
+    res.status(400).send("Error from UserMedicalHistory by Id!");
+  }
 });
 //Create a new one
 router.post(
-    "/addUserMedicalHistory",
-    UserMedicalHistoryValidator,
-    async(req, res) => {
-        try {
-            information = new UserMedicalHistoryModel();
-            user_info = await information.addUserMedicalHistory(req.body);
-            return res.status(200).send(user_info);
-        } catch (err) {
-            return res.status(400).send("Error from addUserMedicalHistory");
-        }
+  "/addUserMedicalHistory",
+  UserMedicalHistoryValidator,
+  async (req, res) => {
+    try {
+      information = new UserMedicalHistoryModel();
+      user_info = await information.addUserMedicalHistory(req.body);
+      return res.status(200).send(user_info);
+    } catch (err) {
+      return res.status(400).send("Error from addUserMedicalHistory");
     }
+  }
 );
 
 router.post(
-    "/addUserMedicalHistory/:id",
-    UserMedicalHistoryValidator,
-    async(req, res) => {
-        try {
-            user_info = await UserMedicalHistoryModel.appendUserMedicalHistory(req.params.id, req.body);
-            return res.status(200).send(user_info);
-        } catch (err) {
-            return res.status(400).send("Error from addUserMedicalHistory");
-        }
+  "/addUserMedicalHistory/:id",
+  UserMedicalHistoryValidator,
+  async (req, res) => {
+    try {
+      user_info = await UserMedicalHistoryModel.appendUserMedicalHistory(
+        req.params.id,
+        req.body
+      );
+      return res.status(200).send(user_info);
+    } catch (err) {
+      return res.status(400).send("Error from appendUserMedicalHistory");
     }
+  }
 );
 
 router.put(
-    "/addUserMedicalHistory",
-    UserMedicalHistoryValidator,
-    async(req, res) => {
-        try {
-            user_info = await UserMedicalHistoryModel.updateUserMedicalHistory(
-                req.body
-            );
-            return res.status(200).send(user_info);
-        } catch (err) {
-            return res.status(400).send("Error from updateUserMedicalHistory");
-        }
+  "/addUserMedicalHistory",
+  UserMedicalHistoryValidator,
+  async (req, res) => {
+    try {
+      user_info = await UserMedicalHistoryModel.updateUserMedicalHistory(
+        req.body
+      );
+      return res.status(200).send(user_info);
+    } catch (err) {
+      return res.status(400).send("Error from updateUserMedicalHistory");
     }
+  }
 );
 
 module.exports = router;
