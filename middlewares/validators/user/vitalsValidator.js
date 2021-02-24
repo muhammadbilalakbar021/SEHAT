47152777f66f4bd72055b990fa80c0bc95715adf
Mian442 <62568869+Mian442@users.noreply.github.com>
2021-02-25 01:54:05 +0500

    Delete vitalsValidator.js

--- middlewares/validators/user/vitalsValidator.js
+++
@@ -1,7 +0,0 @@
-let VitalsModel = require("../../../models/user/VitalsModels");
-module.exports = async function(req, res, next) {
-    const { error } = await VitalsModel.ValidateUserVitals(req.body);
-    if (error) return res.send({ error: error.details[0].message });
-    req.isValidated = true;
-    next();
-};
