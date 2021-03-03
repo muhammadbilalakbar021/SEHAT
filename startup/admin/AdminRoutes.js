c17932713fb6797751606dccf8595f6484bdbbe4
Mian442 <62568869+Mian442@users.noreply.github.com>
2021-03-03 11:13:20 +0500

    Update AdminRoutes.js

--- startup/admin/AdminRoutes.js
+++ startup/admin/AdminRoutes.js
@@ -1,5 +1,4 @@
 const auth = require("../../middlewares/validators/auth/auth");
-const isAdmin = require("../../middlewares/validators/role/isAdmin");
 const adminRouter = require("../../routes/api/v1/admin/AdminRouter");
 module.exports = function (app) {
   app.use("/api/admin", auth, adminRouter);
