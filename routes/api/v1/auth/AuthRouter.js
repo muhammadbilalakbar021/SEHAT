1509f43dd6b8091ad165fc306ab193be9ac47fcd
Mian442 <62568869+Mian442@users.noreply.github.com>
2021-03-03 11:16:48 +0500

    Create AuthRouter.js

---
+++ routes/api/v1/auth/AuthRouter.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const duplicateEmailValidator = require("../../../../middlewares/validators/user/DuplicateEmailValidator");
+const loginValidator = require("../../../../middlewares/validators/user/LoginValidator");
+const UserValidator = require("../../../../middlewares/validators/user/UserValidator");
+const UserModel = require("../../../../models/user/UserModel");
+const router = express.Router();
+
+// Get request for returning User by login
+router.post("/login", loginValidator, async (req, res) => {
+  try {
+    console.log(req.body);
+    const User = await UserModel.getUserByEmailPassword(req.body);
+    return res.status(200).send(User);
+  } catch (err) {
+    return res.status(400).send("Error from User login!");
+  }
+});
+
+// Post request for adding User *checked*
+router.post(
+  "/signup",
+  UserValidator,
+  duplicateEmailValidator,
+  async (req, res) => {
+    try {
+      User = new UserModel();
+      p = await User.addUser(req.body);
+      // Return User list
+      return res.status(200).send(p);
+    } catch (err) {
+      return res.status(400).send(err);
+    }
+  }
+);
+
+module.exports = router;
