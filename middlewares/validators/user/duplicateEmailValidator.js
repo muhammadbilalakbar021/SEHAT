1c7600ff23e68bcaeece1b4aad534f7ebfac5fcd
Mian442 <62568869+Mian442@users.noreply.github.com>
2021-03-03 11:20:09 +0500

    Delete duplicateEmailValidator.js

--- middlewares/validators/user/duplicateEmailValidator.js
+++
@@ -1,9 +0,0 @@
-let UserModel = require("../../../models/user/UserModel");
-module.exports = async function (req, res, next) {
-  let user = await UserModel.findOne({ email: req.body.email });
-  if (user) {
-    return res.status(400).send("Email has been already registered");
-  }
-  req.isValidated = true;
-  next();
-};
